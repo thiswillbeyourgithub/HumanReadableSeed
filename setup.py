d6c0151f30aef4cd99f3250a295e6048bafa6b4d
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-10-12 14:22:27 +0200

    bump version and fix bumpver
    Signed-off-by: thiswillbeyourgithub
    <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="HumanReadableSeed",
-    version="0.0.2",
+    version="0.0.4",
     description="Reversible conversion between seeds and human readable words",
     long_description=long_description,
     long_description_content_type="text/markdown",
