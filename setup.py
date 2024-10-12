0dbb898ab7b34d815f9b8fc6a937959f31da675f
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-10-12 15:03:42 +0200

    bump version 0.0.7 -> 0.0.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="HumanReadableSeed",
-    version="0.0.7",
+    version="0.0.8",
     description="Reversible conversion between seeds and human readable words",
     long_description=long_description,
     long_description_content_type="text/markdown",
